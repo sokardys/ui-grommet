aa75f4e4038f9fb360139fee8be42106bb7cbe25
Javier Ortiz <jortiz@tutellus.com>
2020-04-08 09:10:50 +0200

    feat(Section) added waves for be able to config cool sections

--- src/section/Section.jsx
+++ src/section/Section.jsx
@@ -1,16 +1,24 @@
 import React, { useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import 'jarallax/dist/jarallax.css';
+import 'jarallax/dist/jarallax.css'
 
 import { Box } from 'grommet'
 import { Cta } from '../cta/Cta'
 import { Title } from '../title/Title'
 import { Description } from '../description/Description'
 
+const WaveBox = styled(Box)`
+  width: 100%;
+  & svg {
+    width: 100%;
+  }
+`
+
 export const Section = styled(({
   className,
   children,
+  background = 'light-1',
   width = 'xlarge',
   title,
   titleConfig = {},
@@ -18,32 +26,40 @@ export const Section = styled(({
   descriptionConfig = {},
   parallax,
   parallaxConfig = {},
+  waves = {},
   cta,
   ...props
 }) => {
   const parallaxRef = useRef()
   useEffect(() => {
-    
-    if (parallaxRef.current){
-      const { jarallax } = require('jarallax')
-      jarallax( parallaxRef.current, parallaxConfig )
+    const { jarallax } = require('jarallax')
+    if (parallaxRef.current) {
+      jarallax(parallaxRef.current, parallaxConfig)
     }
     return () => {
-      if (parallaxRef.current){
-        jarallax( parallaxRef.current, 'destroy' );
+      if (parallaxRef.current) {
+        jarallax(parallaxRef.current, 'destroy')
       }
     }
   }, [parallaxRef])
 
   const marginNone = { top: 'none', bottom: 'none', right: 'none', left: 'none' }
-  return (
-    <Box className={className} align='center' pad='large' {...props}>
-      {parallax && 
+  const hasWaves = waves.top || waves.bottom
+
+  const composeSection = () =>
+    <Box
+      className={className}
+      align='center'
+      pad={{ vertical: 'xlarge', horizontal: 'large' }}
+      background={background}
+      {...props}
+    >
+      {parallax &&
         <Box
           className='jarallax'
           ref={parallaxRef}
         >
-          <img className="jarallax-img" src={parallax} />
+          <img className='jarallax-img' src={parallax} />
         </Box>}
       <Box className='content' width={width} flex='grow'>
         <>
@@ -66,7 +82,16 @@ export const Section = styled(({
         </>
       </Box>
     </Box>
-  )
+  if (hasWaves) {
+    return (
+      <Box pad='none' background={background}>
+        {waves.top && <WaveBox dangerouslySetInnerHTML={{ __html: waves.top }} />}
+        {composeSection()}
+        {waves.bottom && <WaveBox dangerouslySetInnerHTML={{ __html: waves.bottom }} />}
+      </Box>
+    )
+  }
+  return composeSection()
 })`
   position: relative;
   overflow: hidden;
--- src/section/Section.stories.js
+++ src/section/Section.stories.js
@@ -78,8 +78,10 @@ export const WithParallax = () =>
       title='Titulo de la Sección'
       parallax='//i.pinimg.com/originals/9c/69/84/9c69843107ad62dde66d3f81993d4eb7.jpg'
     >
-      <Box background='brand' pad='medium'>
-        <Text>Section Header</Text>
+      <Box pad='medium' align='center'>
+        <Text align='center' size='large' color='accent-2' weight='bold'>
+          the more content the section has,<br />the stronger<br />the effect
+        </Text>
       </Box>
     </Section>
     <Section
@@ -89,6 +91,45 @@ export const WithParallax = () =>
     >
       <Box pad='medium'>
         <Text>Other Section</Text>
+      </Box>
+    </Section>
+  </Page>
+
+export const WithWaves = () =>
+  <Page>
+    <Section
+      align='center'
+      title='Section 1'
+      background='light-1'
+    >
+      <Box pad='medium'>
+        <Text>Sample Content</Text>
+      </Box>
+    </Section>
+    <Section
+      align='center'
+      title='Waves Sample'
+      background='dark-1'
+      waves={{
+        top: '<svg viewBox="0 0 1440 160" xmlns="http://www.w3.org/2000/svg"><path fill="#F8F8F8" fill-opacity="1" d="M0,160L80,149.3C160,139,320,117,480,112C640,107,800,117,960,117.3C1120,117,1280,107,1360,101.3L1440,96L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"/></svg>',
+        bottom: '<svg viewBox="0 179.263 1440 140.737" xmlns="http://www.w3.org/2000/svg"><path fill="#7D4CDB" fill-opacity="1" d="M0,256L80,266.7C160,277,320,299,480,282.7C640,267,800,213,960,192C1120,171,1280,181,1360,186.7L1440,192L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"/></svg>'
+      }}
+    >
+      <Box pad='medium'>
+        <Text>
+          Use https://getwaves.io/ to generate the desired shape.
+          <br /><br />
+          After downloading it, you will have to adjust the svg to avoid transparent space. The maximum height should correspond to the wave' height
+        </Text>
+      </Box>
+    </Section>
+    <Section
+      align='center'
+      title='Section 3'
+      background='brand'
+    >
+      <Box pad='medium'>
+        <Text>Sample Content </Text>
       </Box>
     </Section>
   </Page>
