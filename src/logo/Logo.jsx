25d78259b9c0bbd9096b95d79a696493f9deea2c
Javier Ortiz <jortiz@tutellus.com>
2020-04-10 03:11:27 +0200

    feat(Logo) for show an Image on footer [WIP]

---
+++ src/logo/Logo.jsx
@@ -0,0 +1,20 @@
+import {
+  Image
+} from 'ui-grommet'
+
+import {
+  Box
+} from 'grommet'
+
+export const Logo = ({ src, ...props }) =>
+  <Box
+    pad='small'
+    {...props}
+  >
+    <Image
+      lazy={false}
+      fill
+      fit='contain'
+      src={src}
+    />
+  </Box>
