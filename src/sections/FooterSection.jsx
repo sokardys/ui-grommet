b1749ac15282ce7b95f209758c477c5d7a2558ce
Javier Ortiz <jortiz@tutellus.com>
2020-04-14 02:17:34 +0200

    fix/(FooterSection) key problem solved

--- src/sections/FooterSection.jsx
+++ src/sections/FooterSection.jsx
@@ -18,7 +18,7 @@ export const FooterSection = ({
     {links &&
       <Box justify='center' direction='row' gap='medium' margin={{ bottom: 'small' }}>
         {Object.keys(links).map(key =>
-          <Box><Anchor key={`footer-anchor-${key}`} href={key} label={links[key]} size='small' /></Box>
+          <Box key={`footer-anchor-${key}`}><Anchor href={key} label={links[key]} size='small' /></Box>
           )}
       </Box>}
     <Box align='center' dangerouslySetInnerHTML={{ __html: children }} />
