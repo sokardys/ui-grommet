94c6e57692b400dc2bc8457da07290db6c112e40
Javier Ortiz <jortiz@tutellus.com>
2020-05-07 18:22:16 +0200

    feat(CarouselSection) publishing carousel section

--- src/jsonpage/JSONPage.jsx
+++ src/jsonpage/JSONPage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {
+  CarouselSection,
   FloatingHeaderSection,
+  FooterSection,
+  FormSection,
   HeaderSection,
   HeroSection,
-  FormSection,
-  FooterSection,
   MarkdownSection,
   SeoSection
 } from '../sections'
@@ -14,18 +15,20 @@ export const JSONPage = ({ uri, sections = [] }) =>
   <>
     {sections.map(({ section, ...config }, index) => {
       switch (section) {
+        case 'carousel':
+          return <CarouselSection key={`jsonpage-section-${index}`} {...config} />
         case 'floating':
           return <FloatingHeaderSection key={`jsonpage-section-${index}`} {...config} />
-        case 'header':
-          return <HeaderSection key={`jsonpage-section-${index}`} {...config} />
         case 'footer':
           return <FooterSection key={`jsonpage-section-${index}`} {...config} />
+        case 'form':
+          return <FormSection key={`jsonpage-section-${index}`} {...config} />
+        case 'header':
+          return <HeaderSection key={`jsonpage-section-${index}`} {...config} />
         case 'hero':
           return <HeroSection key={`jsonpage-section-${index}`} {...config} />
         case 'markdown':
           return <MarkdownSection key={`jsonpage-section-${index}`} {...config} />
-        case 'form':
-          return <FormSection key={`jsonpage-section-${index}`} {...config} />
         case 'seo':
           return <SeoSection key={`jsonpage-section-${index}`} {...config} uri={uri} />
         default:
